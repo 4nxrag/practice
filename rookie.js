220a09f0083a9aba59b4d6c599b17d6aa7764d8c
Anurag Jha <anuuragjha70@gmail.com>
2025-12-25 20:58:07 +0530

    practicing basics feeling dumb

--- rookie.js
+++ rookie.js
@@ -1,14 +1,71 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7]
+// let numbers = [1, 2, 3, 4, 5, 6, 7]
 
-function countDracula(){
-    let count = 0;
+// function countDracula(){
+//     let count = 0;
 
-    for(let i=0; i<numbers.length; i++) {
-        if(numbers[i]>4){
-            count++
-        }
+//     for(let i=0; i<numbers.length; i++) {
+//         if(numbers[i]>4){
+//             count++
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countDracula());
+
+
+
+// function addArray(arr, value){
+//     arr.push(value);
+
+//     let sum = 0;
+//     for(let i=0; i<arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     return sum
+// }
+
+// let num = [5, 6, 7, 8, 9]
+// let inputNum = Number(prompt("Enter A Number"));
+// console.log(addArray(num, inputNum));
+
+// let ticktimer = 0;
+
+// function tickerx5() {
+//     return new Promise((resolve, reject) =>{
+//         ticktimer++
+//         setTimeout(()=>{
+//             console.log(`"Tick Tick" ${ticktimer}`);
+//             resolve("Shits Working");
+//             reject("You are pretending to be a dev you are a disgrace for real");
+//         },1000)
+//     }
+// )
+// }
+
+// tickerx5()
+// .then((message) =>{
+//     console.log(message); // handles the resolve part
+// })
+
+// .catch((error)=>{
+//     console.error(error); // handles the rejection part
+// })
+
+// .finally(()=>{
+//     console.log("I am running so you dont feel useless!!") // runs either ways
+// })
+
+// for (i=0; i<=4; i++) {
+//     tickerx5();
+// }
+
+
+const button = document.getElementById("startButton");
+let counter = 0
+
+button.addEventListener("click", function()  {
+    for (let i=10; i>=0; i--) {
+        return i;
     }
-    return count;
-}
-
-console.log(countDracula());
+})
